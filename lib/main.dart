47f35d14f287040059318eb39ffd918b580647ba
Rahul Bhardwaj <rahul.03111999@gmail.com>
2022-03-14 19:14:45 +0530

    fixed splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:heroicons/heroicons.dart';
 import 'package:uk_travel_guide/splash.dart';
 
+import 'home.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -20,133 +22,6 @@ class MyApp extends StatelessWidget {
       // ),
       home: Splash(),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int currentIndex = 0;
-
-  final pages = [
-    Scaffold(
-      backgroundColor: const Color(0xffF0F0F0),
-      body: ListView(
-        padding: const EdgeInsets.all(16),
-        children: <Widget>[
-          ElevatedButton(
-            onPressed: () =>
-                Fluttertoast.showToast(msg: "Button Pressed", fontSize: 18),
-            child: const Text("4 Dham Yatra Guide"),
-            style: ElevatedButton.styleFrom(
-                minimumSize: const Size(280, 80),
-                textStyle: const TextStyle(
-                  fontSize: 20,
-                ),
-                primary: const Color(0xffEFEAEA),
-                onPrimary: const Color(0xffE76D0A)),
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () =>
-                Fluttertoast.showToast(msg: "Button Pressed", fontSize: 18),
-            child: const Text("Adventure Guide"),
-            style: ElevatedButton.styleFrom(
-                minimumSize: const Size(280, 80),
-                textStyle: const TextStyle(
-                  fontSize: 20,
-                ),
-                primary: const Color(0xffEFEAEA),
-                onPrimary: const Color(0xffE76D0A)),
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () =>
-                Fluttertoast.showToast(msg: "Button Pressed", fontSize: 18),
-            child: const Text("Sceneric Spots"),
-            style: ElevatedButton.styleFrom(
-                minimumSize: const Size(280, 80),
-                textStyle: const TextStyle(
-                  fontSize: 20,
-                ),
-                primary: const Color(0xffEFEAEA),
-                onPrimary: const Color(0xffE76D0A)),
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () =>
-                Fluttertoast.showToast(msg: "Button Pressed", fontSize: 18),
-            child: const Text("Check Available Hotels"),
-            style: ElevatedButton.styleFrom(
-                minimumSize: const Size(280, 80),
-                textStyle: const TextStyle(
-                  fontSize: 20,
-                ),
-                primary: const Color(0xffEFEAEA),
-                onPrimary: const Color(0xffE76D0A)),
-          ),
-          const SizedBox(height: 20),
-        ],
-      ),
-    ),
-    const Scaffold(
-      backgroundColor: Color(0xffF0F0F0),
-      body: Text("This will be AR View!"),
-    ),
-    Scaffold(
-      backgroundColor: const Color(0xffF0F0F0),
-      body: Center(
-        child: Image.asset("assets/images/art.png"),
-      ),
-    ),
-    const Scaffold(
-      backgroundColor: Color(0xffF0F0F0),
-      body: Text("This will be SOS Page!"),
-    ),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Uttarakhand Tourist Guide"),
-        centerTitle: true,
-        backgroundColor: const Color(0xffE76D0A),
-      ),
-      backgroundColor: const Color(0xffF0F0F0),
-      body: pages[currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.shifting,
-        selectedItemColor: const Color(0xffE76D0A),
-        unselectedItemColor: const Color.fromARGB(255, 235, 173, 123),
-        currentIndex: currentIndex,
-        onTap: (index) => setState(() => currentIndex = index),
-        items: const [
-          BottomNavigationBarItem(
-              icon: HeroIcon(HeroIcons.home),
-              label: "Home",
-              backgroundColor: Color(0xffF0F0F0)),
-          BottomNavigationBarItem(
-              icon: HeroIcon(HeroIcons.camera),
-              label: "AR View",
-              backgroundColor: Color(0xffF0F0F0)),
-          BottomNavigationBarItem(
-              icon: HeroIcon(HeroIcons.locationMarker),
-              label: "Explore",
-              backgroundColor: Color(0xffF0F0F0)),
-          BottomNavigationBarItem(
-              icon: HeroIcon(HeroIcons.exclamationCircle),
-              label: "SOS",
-              backgroundColor: Color(0xffF0F0F0)),
-        ],
-      ),
     );
   }
 }
--- lib/splash.dart
+++ lib/splash.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'main.dart';
+import 'package:uk_travel_guide/home.dart';
 
 class Splash extends StatefulWidget {
-  const Splash({ Key? key }) : super(key: key);
+  const Splash({Key? key}) : super(key: key);
 
   @override
   State<Splash> createState() => _SplashState();
@@ -11,53 +10,54 @@ class Splash extends StatefulWidget {
 
 class _SplashState extends State<Splash> {
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    _navigatetohome();
+    _navigateToHome();
   }
 
-  _navigatetohome() async {
-    await Future.delayed(Duration(milliseconds: 1500), () {});
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>MyHomePage(title: 'Splash',)));
+  _navigateToHome() async {
+    await Future.delayed(const Duration(milliseconds: 1500), () {});
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) =>
+                const MyHomePage(title: "Uttarakhand Tourist Guide")));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
-      body:ListView(
+      body: ListView(
         children: [
           Container(
-            margin: EdgeInsets.symmetric(vertical:20),
-            child:Image.asset(
-              'assests/images/uttarakhand-logo-english.png',
+            margin: EdgeInsets.symmetric(vertical: 20),
+            child: Image.asset(
+              'assets/images/uttarakhand-logo-english.png',
               width: 100,
               height: 100,
             ),
           ),
           Container(
-        margin: EdgeInsets.symmetric(horizontal: 50, vertical:20),
-        child: Center(
-          child: Text(
-          'Uttarakhand Tourism App',
-          textAlign: TextAlign.center,
-          style:TextStyle(
-            fontSize:50,
-            fontWeight:FontWeight.bold,
-            color: Colors.purple,
+            margin: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+            child: const Center(
+              child: Text(
+                'Uttarakhand Tourism App',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.purple,
+                ),
+              ),
+            ),
           ),
-        ),
-        ),
-        
-        
-      ),
-
-      Image.asset(
-        'assests/images/mountain.png'
-      ),
+          Image.asset(
+            'assets/images/mountain.png',
+            height: 650,
+          ),
         ],
       ),
-      
     );
   }
 }
